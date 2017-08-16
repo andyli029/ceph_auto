2abd0b49e5cdf8948c54ef2ca3eb80a935b24698
zhiangli <zhiangli029@gmail.com>
2017-08-16 19:02:23 +0800

    modify create and add add_mon.sh

--- zetta/add_mon/add_mon.sh
+++ zetta/add_mon/add_mon.sh
@@ -8,62 +8,14 @@ do
 	ip=$prefix.$i
 	echo "$ip"
 
-        sed -i "s/mon initial.*$/mon initial members = node$i/g" ./ceph.conf
-        sed -i "s/mon host.*$/mon host = $prefix.$i/g" ./ceph.conf
-        sed -i "s/public network.*$/public network = $prefix.0\/24/g" ./ceph.conf
-        if [[ `echo $?` != 0 ]]
-        then
-                echo "fix ceph.conf error."
-                break
-        else
-                echo "fix ceph.conf success."
-        fi
 
-	cp ./ceph.conf /etc/ceph/ceph.conf
+	rm -f /etc/ceph/ceph.conf
 	if [[ `echo $?` != 0 ]]
         then
                 echo "cp ceph.conf error."
                 break
         else
                 echo "cp ceph.conf success."
-        fi
-
-
-	ceph-authtool --create-keyring /tmp/ceph.mon.keyring --gen-key -n mon. --cap mon 'allow *'
-        if [[ `echo $?` != 0 ]]
-        then
-                echo "8 error."
-                break
-        else
-                echo "8 success."
-        fi
-
-		#/etc/init.d/ceph stop osd.$i
-        ceph-authtool --create-keyring /etc/ceph/ceph.client.admin.keyring --gen-key -n client.admin --set-uid=0 --cap mon 'allow *' --cap osd 'allow *' --cap mds 'allow'
-	if [[ `echo $?` != 0 ]]
-        then
-                echo "9 error."
-                break
-        else
-                echo "9 success."
-        fi
-
-	ceph-authtool /tmp/ceph.mon.keyring --import-keyring /etc/ceph/ceph.client.admin.keyring
-        if [[ `echo $?` != 0 ]]
-        then
-                echo "10 error."
-                break
-        else
-                echo "10 success."
-        fi
-
-	monmaptool --create --add node$i $ip --fsid a7f64266-0894-4f1e-a635-d0lizha0e993 /tmp/monmap	
-        if [[ `echo $?` != 0 ]]
-        then
-                echo "11 error."
-                break
-        else
-                echo "11 success."
         fi
 
         mkdir /var/lib/ceph/mon/ceph-node$i
@@ -74,18 +26,8 @@ do
         else
                 echo "12 success."
         fi
-.<<block
-	chown ceph:ceph -R /var/lib/ceph/mon/ceph-node$i
-	if [[ `echo $?` != 0 ]]
-	then
-                echo "12.1 error."
-                break
-        else
-                echo "12.1 success."
-        fi
-block
 
-	ceph-mon --mkfs -i node$i --monmap /tmp/monmap --keyring /tmp/ceph.mon.keyring
+	ceph auth get mon. -o /tmp/ceph.mon.keyring
         if [[ `echo $?` != 0 ]]
         then
                 echo "13 error."
@@ -94,7 +36,16 @@ block
                 echo "13 success."
         fi
 
-        touch /var/lib/ceph/mon/ceph-node$i/done
+        ceph mon getmap -o /tmp/monmap
+        if [[ `echo $?` != 0 ]]
+        then
+                echo "14 error."
+                break
+        else
+                echo "14 success."
+        fi
+
+	ceph-mon --mkfs -i node$i --monmap /tmp/monmap --keyring /tmp/ceph.mon.keyring
         if [[ `echo $?` != 0 ]]
         then
                 echo "15 error."
@@ -112,6 +63,25 @@ block
                 echo "15.1 success."
         fi
 
+	mon add node$i $ip
+        if [[ `echo $?` != 0 ]]
+        then
+                echo "16 error."
+                break
+        else
+                echo "16 success."
+        fi
+
+	ceph-mon -i node$i --public-addr $ip
+        if [[ `echo $?` != 0 ]]
+        then
+                echo "17 error."
+                break
+        else
+                echo "17 success."
+        fi
+
+.<<block
 	#systemctl reset-failed ceph-mon@node$i.service
 	systemctl start ceph-mon@node$i
         if [[ `echo $?` != 0 ]]
@@ -133,4 +103,6 @@ block
 
 	ps -ef |grep ceph-mon
 	#ceph osd lspools && ceph -s && ps -ef |grep ceph-mon
+block
+
 done
